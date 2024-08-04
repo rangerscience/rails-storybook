bc9d4fe74fcdb41119270261885f0e8ef6210357
Nicholas Barone <nicktbarone@gmail.com>
2024-08-04 13:06:29 -0700

    bundle gem rails-storybook

---
+++ lib/rails/storybook.rb
@@ -0,0 +1,8 @@
+require "rails/storybook/version"
+
+module Rails
+  module Storybook
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/rails/storybook_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Rails::Storybook do
+  it "has a version number" do
+    expect(Rails::Storybook::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
